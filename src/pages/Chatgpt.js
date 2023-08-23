8e5735c98c3dd8f267d938b7996f7f7b11b9c7a8
earthpowerful <earthpowerful.ep@gmail.com>
2023-08-23 17:34:48 +0900

    update first message of chatGPT

--- src/pages/Chatgpt.js
+++ src/pages/Chatgpt.js
@@ -4,91 +4,96 @@ import avatar2 from "../assets/image/chatGPT_avatar.png";
 import axios from "axios";
 
 export default function Chatgpt() {
-  const [messages, setMessages] = useState([
-    { role: "system", content: "ChatGPT joined the conversation." },
-  ]);
-
-  // const [userPrompt, setUserPrompt] = useState("");
+  const [messages, setMessages] = useState([]);
   const [inputText, setInputText] = useState("");
+  const [hasJoinedMessageDisplayed, setHasJoinedMessageDisplayed] =
+    useState(false);
 
   const handleInputChange = (e) => {
     setInputText(e.target.value);
   };
 
   useEffect(() => {
-    const textBoard = document.getElementById("pad");
+    if (!hasJoinedMessageDisplayed) {
+      setMessages([
+        { role: "assistant", content: "ChatGPT joined the conversation." },
+      ]);
+      setHasJoinedMessageDisplayed(true);
+    } else {
+      const textBoard = document.getElementById("pad");
 
-    if (messages[messages.length - 1].role === "user") {
-      const user = document.createElement("div");
-      user.className = "grid grid-cols-[1fr_auto] ml-[1050px] pb-[10px] gap-2";
+      if (messages[messages.length - 1].role === "user") {
+        const user = document.createElement("div");
+        user.className =
+          "grid grid-cols-[1fr_auto] ml-[1050px] pb-[10px] gap-2";
 
-      const userText = document.createElement("div");
-      userText.className = "flex justify-end";
+        const userText = document.createElement("div");
+        userText.className = "flex justify-end";
 
-      const userText_content = document.createElement("div");
-      userText_content.className =
-        "inline-grid  rounded-[15px_15px_0px_15px] bg-[#d8d8d8]";
-      userText.appendChild(userText_content);
+        const userText_content = document.createElement("div");
+        userText_content.className =
+          "inline-grid  rounded-[15px_15px_0px_15px] bg-[#d8d8d8]";
+        userText.appendChild(userText_content);
 
-      const userText_h4 = document.createElement("h4");
-      userText_h4.className = "text-[#253C6E] p-3";
-      userText_h4.innerHTML = messages[messages.length - 1].content;
+        const userText_h4 = document.createElement("h4");
+        userText_h4.className = "text-[#253C6E] p-3";
+        userText_h4.innerHTML = messages[messages.length - 1].content;
 
-      userText_content.appendChild(userText_h4);
+        userText_content.appendChild(userText_h4);
 
-      const userAvatar = document.createElement("div");
-      userAvatar.className = "w-[50px] self-end mr-3";
+        const userAvatar = document.createElement("div");
+        userAvatar.className = "w-[50px] self-end mr-3";
 
-      const avatar_img = document.createElement("img");
-      avatar_img.className = "rounded-[50%]";
-      avatar_img.src = avatar1;
+        const avatar_img = document.createElement("img");
+        avatar_img.className = "rounded-[50%]";
+        avatar_img.src = avatar1;
 
-      userAvatar.appendChild(avatar_img);
+        userAvatar.appendChild(avatar_img);
 
-      user.appendChild(userText);
-      user.appendChild(userAvatar);
+        user.appendChild(userText);
+        user.appendChild(userAvatar);
 
-      textBoard.appendChild(user);
-    } else if (messages[messages.length - 1].role === "assistant") {
-      const aiReply = document.createElement("div");
-      aiReply.className =
-        "grid grid-cols-[auto_1fr] pb-[10px] mr-[1050px] gap-2";
+        textBoard.appendChild(user);
+      } else if (messages[messages.length - 1].role === "assistant") {
+        const aiReply = document.createElement("div");
+        aiReply.className =
+          "grid grid-cols-[auto_1fr] pb-[10px] mr-[1050px] gap-2";
 
-      const aiReply_content = document.createElement("div");
-      aiReply_content.className = "w-[50px] self-end ml-3";
+        const aiReply_content = document.createElement("div");
+        aiReply_content.className = "w-[50px] self-end ml-3";
 
-      const aiAvatar = document.createElement("img");
-      aiAvatar.className = "rounded-[50%]";
-      aiAvatar.src = avatar2;
+        const aiAvatar = document.createElement("img");
+        aiAvatar.className = "rounded-[50%]";
+        aiAvatar.src = avatar2;
 
-      aiReply_content.appendChild(aiAvatar);
-      aiReply.appendChild(aiReply_content);
+        aiReply_content.appendChild(aiAvatar);
+        aiReply.appendChild(aiReply_content);
 
-      const aiText = document.createElement("div");
+        const aiText = document.createElement("div");
 
-      const aiText_content = document.createElement("div");
-      aiText_content.className =
-        "inline-grid border-[2px] border-[white] rounded-[15px_15px_15px_0px] bg-[#253C6E]";
+        const aiText_content = document.createElement("div");
+        aiText_content.className =
+          "inline-grid border-[2px] border-[white] rounded-[15px_15px_15px_0px] bg-[#253C6E]";
 
-      const aiText_h4 = document.createElement("div");
-      aiText_h4.className = "text-white p-3";
-      aiText_h4.textContent = messages[messages.length - 1].content;
+        const aiText_h4 = document.createElement("div");
+        aiText_h4.className = "text-white p-3";
+        aiText_h4.textContent = messages[messages.length - 1].content;
 
-      aiText_content.appendChild(aiText_h4);
+        aiText_content.appendChild(aiText_h4);
 
-      aiText.appendChild(aiText_content);
-      aiReply.appendChild(aiText);
+        aiText.appendChild(aiText_content);
+        aiReply.appendChild(aiText);
 
-      textBoard.appendChild(aiReply);
+        textBoard.appendChild(aiReply);
+      }
     }
-  }, [messages]);
+  }, [messages, hasJoinedMessageDisplayed]);
 
   const handleSendMessage = async () => {
     if (inputText.trim() === "") return;
 
     const newMessages = [...messages, { role: "user", content: inputText }];
     setMessages(newMessages);
-    // setUserPrompt(inputText);
     setInputText("");
 
     try {
@@ -104,7 +109,7 @@ export default function Chatgpt() {
         {
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer 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`,
+            Authorization: `Bearer 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`,
           },
         }
       );
